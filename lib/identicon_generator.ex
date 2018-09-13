2614d39e2b97e7a8df4a9424d7b205138511be1f
Haydenmarx <haydenmarx@gmail.com>
2018-09-13 14:00:53 -0700

    Added application

---
+++ lib/identicon_generator.ex
@@ -0,0 +1,94 @@
+defmodule IdenticonGenerator do
+  @moduledoc """
+  Documentation for IdenticonGenerator.
+  """
+
+  @doc """
+  Generates indenticons.
+
+  ## Examples
+
+      iex> IdenticonGenerator.hash_input("Hayden")
+      :%Identicon.Image{
+        color: nil,
+        grid: nil,
+        hex: [148, 180, 14, 255, 245, 194, 211, 91, 98, 67, 120, 76, 136, 175, 232],
+        pixel_map: nil
+      }
+
+      iex> IdenticonGenerator.main("Hayden")
+  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    |> Enum.slice(0, 15)
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image| color: {r, g, b }}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    grid =
+      hex_list
+      |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail ] = row
+    row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+  
+end
---
+++ lib/image.ex
@@ -0,0 +1,5 @@
+defmodule Identicon.Image do
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
+end
+##no methods should be attached here
+## only primitive data
